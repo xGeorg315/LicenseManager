c202f903a1804f055e813d342b379ebbd1d6833b
xGeorg315 <georg9675@gmail.com>
2023-12-27 18:00:32 +0100

    Init

---
+++ licensemanager/src/main/java/com/keyman/licensemanager/LicensemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.keyman.licensemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensemanagerApplication.class, args);
+	}
+
+}
---
+++ licensemanager/src/test/java/com/keyman/licensemanager/LicensemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyman.licensemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
