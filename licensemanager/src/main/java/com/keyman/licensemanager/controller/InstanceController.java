0b8d33b76420b1c28da3529ec1e2171594a10b11
SalenH <116958113+SalenH@users.noreply.github.com>
2024-01-08 22:27:17 +0100

    Update InstanceController.java

--- licensemanager/src/main/java/com/keyman/licensemanager/controller/InstanceController.java
+++ licensemanager/src/main/java/com/keyman/licensemanager/controller/InstanceController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/admin/instances")
+@RequestMapping("/instances")
 public class InstanceController {
 
     @Autowired
